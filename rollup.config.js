3ffa6d8845421ed787e08b7b83ee3f47cf3c3246
Aleksandr Grenishin <nd0ut.me@gmail.com>
2018-06-26 13:30:52 +0300

    add minified iife bundle for browsers

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,9 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
+import {terser} from 'rollup-plugin-terser'
 
-const getPlugins = ({forBrowser} = {}) =>
+const getPlugins = ({forBrowser, uglify, banner} = {}) =>
   [
     replace({'process.env.NODE_ENV': process.env.NODE_ENV}),
     forBrowser && resolve({browser: true}),
@@ -13,14 +14,16 @@ const getPlugins = ({forBrowser} = {}) =>
       sourceMap: false,
     }),
     babel(),
-    license({
-      banner: `
-      <%= pkg.name %> <%= pkg.version %>
-      <%= pkg.description %>
-      <%= pkg.homepage %>
-      Date: <%= moment().format('YYYY-MM-DD') %>
-    `,
-    }),
+    uglify && terser(),
+    banner &&
+      license({
+        banner: `
+          <%= pkg.name %> <%= pkg.version %>
+          <%= pkg.description %>
+          <%= pkg.homepage %>
+          Date: <%= moment().format('YYYY-MM-DD') %>
+        `,
+      }),
   ].filter(plugin => !!plugin)
 
 export default [
@@ -32,25 +35,45 @@ export default [
       {
         file: 'dist/uploadcare.cjs.js',
         format: 'cjs',
+        interop: false,
       },
       {
         file: 'dist/uploadcare.esm.js',
         format: 'esm',
+        interop: false,
       },
     ],
   },
   {
     input: 'src/index.js',
     plugins: getPlugins({forBrowser: true}),
+    external: ['axios', 'query-string', 'form-data'],
     output: [
       {
-        file: 'dist/uploadcare.browser.umd.js',
-        name: 'uploadcareAPI',
-        format: 'umd',
+        file: 'dist/uploadcare.browser.cjs.js',
+        format: 'cjs',
+        interop: false,
       },
       {
         file: 'dist/uploadcare.browser.esm.js',
         format: 'esm',
+        interop: false,
+      },
+    ],
+  },
+  {
+    input: 'src/index.js',
+    plugins: getPlugins({
+      forBrowser: true,
+      uglify: true,
+      banner: true,
+    }),
+    output: [
+      {
+        file: 'dist/uploadcare.browser.iife.js',
+        format: 'iife',
+        name: 'uploadcareAPI',
+        interop: false,
       },
     ],
   },
