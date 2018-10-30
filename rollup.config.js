07c66d1df9d5a59708ec6bb255f571e49c74431b
Zarema <zarema.khalilova@gmail.com>
2018-10-30 15:27:45 +0300

    Update build, fix targets (use browserslist only), remove minification from common files (minification must be separate for min files)

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,18 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
-import {terser} from 'rollup-plugin-terser'
 import {sizeSnapshot} from 'rollup-plugin-size-snapshot'
 
-const getPlugins = ({iife} = {}) =>
+const getPlugins = (format) =>
   [
     replace({'process.env.NODE_ENV': process.env.NODE_ENV}),
-    resolve({browser: iife}),
-    iife &&
+    resolve({browser: format === 'iife'}),
+    (format === 'iife') &&
       commonjs({
         include: 'node_modules/**',
         sourceMap: false,
       }),
     babel(),
-    iife && terser(),
     license({
       banner: `
           <%= pkg.name %> <%= pkg.version %>
@@ -31,8 +29,8 @@ const getPlugins = ({iife} = {}) =>
 export default [
   {
     input: 'src/index.js',
-    plugins: getPlugins({iife: false}),
-    external: ['axios', 'query-string', 'form-data'],
+    plugins: getPlugins('esm'),
+    external: ['axios', 'form-data'],
     output: [
       {
         file: 'dist/upload-api.esm.js',
@@ -43,8 +41,8 @@ export default [
   },
   {
     input: 'src/index.js',
-    plugins: getPlugins({iife: false}),
-    external: ['axios', 'query-string', 'form-data'],
+    plugins: getPlugins('cjs'),
+    external: ['axios', 'form-data'],
     output: [
       {
         file: 'dist/upload-api.cjs.js',
@@ -55,7 +53,7 @@ export default [
   },
   {
     input: 'src/index.js',
-    plugins: getPlugins({iife: true}),
+    plugins: getPlugins('iife'),
     output: [
       {
         file: 'dist/upload-api.js',
