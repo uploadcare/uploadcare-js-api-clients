1ef14e4683e2590d83adb932522d5bb8b9409df3
Zarema <zarema.khalilova@gmail.com>
2018-10-31 13:22:24 +0300

    Add minification of iife bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,10 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
+import {terser} from 'rollup-plugin-terser'
 import {sizeSnapshot} from 'rollup-plugin-size-snapshot'
 
-const getPlugins = (format) =>
+const getPlugins = (format, minify = false) =>
   [
     replace({'process.env.NODE_ENV': process.env.NODE_ENV}),
     resolve({browser: format === 'iife'}),
@@ -15,6 +16,7 @@ const getPlugins = (format) =>
         sourceMap: false,
       }),
     babel(),
+    minify && terser({sourcemap: false}),
     license({
       banner: `
           <%= pkg.name %> <%= pkg.version %>
@@ -26,7 +28,6 @@ const getPlugins = (format) =>
     sizeSnapshot(),
   ].filter(plugin => !!plugin)
 
-/* TODO Add creating minified files with `.min` suffix */
 export default [
   {
     input: 'src/index.js',
@@ -58,6 +59,18 @@ export default [
     output: [
       {
         file: 'dist/uploadcare-upload-api.js',
+        format: 'iife',
+        name: 'uploadcareAPI',
+        interop: false,
+      },
+    ],
+  },
+  {
+    input: 'src/index.js',
+    plugins: getPlugins('iife', true),
+    output: [
+      {
+        file: 'dist/uploadcare-upload-api.min.js',
         format: 'iife',
         name: 'uploadcareAPI',
         interop: false,
