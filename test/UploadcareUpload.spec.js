4651fd395cacc1ae87bbc1faeade8ed6f1758a5c
jeetiss <za5rubley@gmail.com>
2018-11-15 11:52:29 +0300

    fix node test, remove unneeded dependencies

--- test/UploadcareUpload.spec.js
+++ test/UploadcareUpload.spec.js
@@ -3,42 +3,34 @@ import UploadcareUpload from '../src/UploadcareUpload'
 describe('UploadcareUpload', () => {
   describe('should request to the right base url', () => {
     it('with default settings', async() => {
-      expect.assertions(1)
-
       const upload = new UploadcareUpload()
 
       const response = await upload.api.request({path: '/info/'})
 
-      expect(response.request.connection.servername).toBe('upload.uploadcare.com')
+      expect(response.config.baseURL).toBe('https://upload.uploadcare.com')
     })
     it('with constructor settings', async() => {
-      expect.assertions(1)
-
       const upload = new UploadcareUpload({baseURL: 'https://upload.staging0.uploadcare.com'})
 
       const response = await upload.api.request({path: '/info/'})
 
-      expect(response.request.connection.servername).toBe('upload.staging0.uploadcare.com')
+      expect(response.config.baseURL).toBe('https://upload.staging0.uploadcare.com')
     })
     it('with setSettings method', async() => {
-      expect.assertions(1)
-
       const upload = new UploadcareUpload()
 
       upload.setSettings({baseURL: 'https://upload.staging0.uploadcare.com'})
 
       const response = await upload.api.request({path: '/info/'})
 
-      expect(response.request.connection.servername).toBe('upload.staging0.uploadcare.com')
+      expect(response.config.baseURL).toBe('https://upload.staging0.uploadcare.com')
     })
     it('with settings as method parameter', async() => {
-      expect.assertions(1)
-
       const upload = new UploadcareUpload()
 
       const response = await upload.api.request({path: '/info/'}, {baseURL: 'https://upload.staging0.uploadcare.com'})
 
-      expect(response.request.connection.servername).toBe('upload.staging0.uploadcare.com')
+      expect(response.config.baseURL).toBe('https://upload.staging0.uploadcare.com')
     })
   })
 })
