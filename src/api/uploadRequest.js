35f801289af6b519e2644657b2a588555af488ca
Zarema <zarema.khalilova@gmail.com>
2018-12-17 18:44:29 +0300

    Update upload request, change types, add custom cancel error

--- src/api/uploadRequest.js
+++ src/api/uploadRequest.js
@@ -1,8 +1,7 @@
 /* @flow */
-import {CancelToken} from 'axios'
+import {CancelToken, isCancel} from 'axios'
 import request from './request'
-import type {RequestConfig, RequestResponse} from './request'
-import type {UploadcareSettings} from '../types'
+import type {RequestOptions, RequestResponse} from './request'
 
 export type UploadProgressEvent = {
   status: 'uploading' | 'uploaded' | 'canceled' | 'error',
@@ -23,19 +22,31 @@ export type Uploading = {|
 /**
  * Performs uploading request to Uploadcare Upload API. Can be canceled and has progress.
  *
- * @param config
- * @param settings
+ * @param {RequestOptions} options – The options for making requests.
+ * @param {string} [options.method=GET] – The request method.
+ * @param {string} options.path – The path to requested method, without endpoint and with slashes.
+ * @param {Object} [options.query] – The URL parameters.
+ * @param {Object} [options.body] – The data to be sent as the body. Only for 'PUT', 'POST', 'PATCH'.
+ * @param {Object} [options.headers] – The custom headers to be sent.
+ * @param {string} [options.baseURL] – The Upload API endpoint.
+ * @param {string} [options.userAgent] – The info about a library that use this request.
  * @return {{promise: Promise<RequestResponse>, onProgress: null, onCancel: null, cancel}}
  */
-export default function uploadRequest(config: RequestConfig, settings: UploadcareSettings = {}): Uploading {
+export default function uploadRequest(options: RequestOptions): Uploading {
   const source = CancelToken.source()
 
   return {
-    promise: request({
-      ...config,
-      onUploadProgress: (progressEvent) => progressEvent,
-      cancelToken: source.token,
-    }, settings),
+    promise: new Promise((resolve, reject) => {
+      request({
+        ...options,
+        onUploadProgress: (progressEvent) => progressEvent,
+        cancelToken: source.token,
+      })
+        .then(response => resolve(response))
+        .catch(error => {
+          reject(isCancel(error) ? 'Request canceled' : error)
+        })
+    }),
     onProgress: null,
     onCancel: null,
     cancel: source.cancel,
