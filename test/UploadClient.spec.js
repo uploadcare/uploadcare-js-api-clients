22525b8fd2210e183c25fa23c3635c674e968f55
Zarema <zarema.khalilova@gmail.com>
2018-12-25 23:28:51 +0300

    Update tests for UploadClient with new errors

--- test/UploadClient.spec.js
+++ test/UploadClient.spec.js
@@ -3,38 +3,61 @@ import * as factory from './fixtureFactory'
 
 describe('UploadClient', () => {
   describe('should request to the right base url', () => {
+    const requestOptions = {
+      path: '/info/',
+      query: {
+        pub_key: factory.publicKey('image'),
+        file_id: factory.uuid('image'),
+      },
+    }
+
     it('with default settings', async() => {
       const upload = new UploadClient()
 
-      const response = await upload.api.request({path: '/info/'})
+      const req = upload.api.request(requestOptions)
 
-      expect(response.url).toBe('https://upload.uploadcare.com/info/')
+      await expectAsync(req).toBeResolved()
+      req.then(({url}) => {
+        expect(url).toBe('https://upload.uploadcare.com/info/')
+      })
     })
     it('with constructor settings', async() => {
       const upload = new UploadClient({baseURL: 'https://upload.staging0.uploadcare.com'})
 
-      const response = await upload.api.request({path: '/info/'})
+      const req = upload.api.request(requestOptions)
 
-      expect(response.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      await expectAsync(req).toBeRejected()
+      req.catch(error => {
+        expect(error.name).toBe('UploadcareError')
+        expect(error.request.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      })
     })
     it('with setSettings method', async() => {
       const upload = new UploadClient()
 
       upload.setSettings({baseURL: 'https://upload.staging0.uploadcare.com'})
 
-      const response = await upload.api.request({path: '/info/'})
+      const req = upload.api.request(requestOptions)
 
-      expect(response.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      await expectAsync(req).toBeRejected()
+      req.catch(error => {
+        expect(error.name).toBe('UploadcareError')
+        expect(error.request.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      })
     })
     it('with settings as options', async() => {
       const upload = new UploadClient()
 
-      const response = await upload.api.request({
-        path: '/info/',
+      const req = upload.api.request({
+        ...requestOptions,
         baseURL: 'https://upload.staging0.uploadcare.com',
       })
 
-      expect(response.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      await expectAsync(req).toBeRejected()
+      req.catch(error => {
+        expect(error.name).toBe('UploadcareError')
+        expect(error.request.url).toBe('https://upload.staging0.uploadcare.com/info/')
+      })
     })
   })
 
