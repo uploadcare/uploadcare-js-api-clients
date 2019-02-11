797d7cac36a7c70b31739988d33eb41cbac1f66e
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-02-11 15:57:15 +0300

    Update tests

--- test/api/fromUrl.spec.js
+++ test/api/fromUrl.spec.js
@@ -1,5 +1,5 @@
-import * as factory from '../_fixtureFactory'
 import fromUrl from '../../src/api/fromUrl'
+import * as factory from '../_fixtureFactory'
 
 describe('API - from url', () => {
   it('should return token for file', async() => {
@@ -10,6 +10,26 @@ describe('API - from url', () => {
 
   it('should be rejected with bad options', (done) => {
     fromUrl(factory.imageUrl('valid'), {publicKey: ''})
+      .then(() => done.fail())
+      .catch(error => {
+        (error.name === 'UploadcareError')
+          ? done()
+          : done.fail(error)
+      })
+  })
+
+  it('should be rejected with image that does not exists', (done) => {
+    fromUrl(factory.imageUrl('doesNotExist'), {publicKey: factory.publicKey('demo')})
+      .then(() => done.fail())
+      .catch(error => {
+        (error.name === 'UploadcareError')
+          ? done()
+          : done.fail(error)
+      })
+  })
+
+  it('should be rejected with image from private IP', (done) => {
+    fromUrl(factory.imageUrl('privateIP'), {publicKey: factory.publicKey('demo')})
       .then(() => done.fail())
       .catch(error => {
         (error.name === 'UploadcareError')
--- test/api/fromUrlStatus.spec.js
+++ test/api/fromUrlStatus.spec.js
@@ -1,11 +1,21 @@
-import * as factory from '../_fixtureFactory'
 import fromUrlStatus from '../../src/api/fromUrlStatus'
+import * as factory from '../_fixtureFactory'
 
 describe('API - from url status', () => {
   it('should return info about file uploaded from url', async() => {
     const data = await fromUrlStatus(factory.token('valid'))
 
     expect(data.uuid).toBeTruthy()
+  })
+
+  it('should be rejected with bad options', (done) => {
+    fromUrlStatus(factory.token('valid'), {publicKey: ''})
+      .then(() => done.fail())
+      .catch(error => {
+        (error.name === 'UploadcareError')
+          ? done()
+          : done.fail(error)
+      })
   })
 
   it('should be rejected with empty token', (done) => {
