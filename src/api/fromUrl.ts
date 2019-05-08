825a72dc0afdcef924e0ab36e3d4907d78711646
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-05-08 17:00:07 +0800

    Remove source from from_url query

--- src/api/fromUrl.ts
+++ src/api/fromUrl.ts
@@ -45,7 +45,6 @@ const getRequestQuery = (sourceUrl: Url, settings: Settings) => ({
   save_URL_duplicates: settings.saveUrlForRecurrentUploads ? 1 : 0,
   signature: settings.secureSignature || '',
   expire: settings.secureExpire || '',
-  source: 'url',
 })
 
 const getRequestOptions = (sourceUrl: Url, settings: Settings): RequestOptions => {
