26e1b415bbd80d02ab1cf9a8f6c81403f9b21367
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-05-20 20:32:31 +0800

    Update imports

--- mock-server/server.ts
+++ mock-server/server.ts
@@ -1,33 +1,37 @@
-const Koa = require('koa')
-const route = require('koa-route')
-
-const app = new Koa()
-
+import Koa from 'koa'
+import * as route from 'koa-route'
 import * as base from './controllers/base'
 import * as fromUrl from './controllers/from_url'
 import * as info from './controllers/info'
 import addTrailingSlashes from 'koa-add-trailing-slashes'
 
+// Middleware
 // import auth from './middleware/auth'
 // import multipart from './middleware/multipart'
 
 import {PORT} from './config'
 
+const app = new Koa()
+
 // Use middleware
 app.use(addTrailingSlashes())
 // app.use(multipart)
 // app.use(auth)
+
+// Routes
 app.use(route.post('/base', base.index))
 app.use(route.post('/from_url', fromUrl.index))
 app.use(route.get('/from_url/status', fromUrl.status))
 app.use(route.get('/info', info.uuid))
 
+// Handle errors
 app.on('error', (err, ctx) => {
   console.error('💔 Server error:')
   console.error(err)
   console.error(ctx)
 });
 
+// Listen server
 app.listen(PORT, () => {
   console.log(`🚀 Server started at http://localhost:${PORT}`)
 })
