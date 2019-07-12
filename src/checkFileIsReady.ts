32ca065793801ab87a2ad95751e63d643e767f3e
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-07-12 12:35:52 +0800

    Fix tests

--- src/checkFileIsReady.ts
+++ src/checkFileIsReady.ts
@@ -3,14 +3,14 @@ import {Settings} from './types'
 import poll, {DEFAULT_TIMEOUT, PollPromiseInterface} from './tools/poll'
 import {Uuid} from './api/types'
 
-type CheckFileIsReadyParams = {
+type FileIsReadyParams = {
   uuid: Uuid,
   timeout?: number,
   onProgress?: Function,
   settings?: Settings
 }
 
-const checkFileIsReady = ({uuid, timeout = DEFAULT_TIMEOUT, onProgress, settings = {}}: CheckFileIsReadyParams): PollPromiseInterface<InfoResponse> =>
+const checkFileIsReady = ({uuid, timeout = DEFAULT_TIMEOUT, onProgress, settings = {}}: FileIsReadyParams): PollPromiseInterface<InfoResponse> =>
   poll<InfoResponse>(
     async () => {
       const response = await info(uuid, settings)
