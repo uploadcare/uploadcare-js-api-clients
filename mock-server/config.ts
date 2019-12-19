53d412d354bef0f121d7cb03c74fc6f3f66311c5
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2019-12-19 14:03:39 +0300

    Add new `publicKey` for multipart testing

--- mock-server/config.ts
+++ mock-server/config.ts
@@ -1,5 +1,9 @@
 const PORT = 3000
 
-const ALLOWED_PUBLIC_KEYS = ['demopublickey', '657ec3b474e01b9045f7']
+const ALLOWED_PUBLIC_KEYS = [
+  'demopublickey',
+  '657ec3b474e01b9045f7',
+  'pub_test__no_storing',
+]
 
 export { PORT, ALLOWED_PUBLIC_KEYS }
