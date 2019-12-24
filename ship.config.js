4f719baa0f8b43716a7622bf78de434a36aebf9c
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2019-12-24 14:25:34 +0300

    chore: update library version on release (#126)
    * move version to separate file
    * add script for updating version

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
-  mergeStrategy: { toSameBranch: ['master'] }
+  mergeStrategy: { toSameBranch: ['master'] },
+  versionUpdated: ({ version, dir }) => {
+    const versionPath = path.resolve(dir, 'src/version.ts');
+    fs.writeFileSync(versionPath, `export default '${version}'\n`);
+  }
 }
