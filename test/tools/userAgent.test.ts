e3cb79b5ee304226d509d6082af20ad232ed55e2
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2019-12-25 12:12:36 +0300

    test: use version from source in user agent tests (#133)

--- test/tools/userAgent.test.ts
+++ test/tools/userAgent.test.ts
@@ -1,26 +1,28 @@
-// import version from '../../src/version'
+import version from '../../src/version'
 import { getUserAgent } from '../../src/tools/userAgent'
 
 describe('getUserAgent', () => {
   it('should generate user agent without params', () => {
-    expect(getUserAgent()).toBe('UploadcareUploadClient/1.0.0 (JavaScript)')
+    expect(getUserAgent()).toBe(
+      `UploadcareUploadClient/${version} (JavaScript)`
+    )
   })
 
   it('should generate user agent with integration', () => {
     expect(getUserAgent({ integration: 'test' })).toBe(
-      'UploadcareUploadClient/1.0.0 (JavaScript; test)'
+      `UploadcareUploadClient/${version} (JavaScript; test)`
     )
   })
 
   it('should generate user agent with pub-key', () => {
     expect(getUserAgent({ publicKey: 'test' })).toBe(
-      'UploadcareUploadClient/1.0.0/test (JavaScript)'
+      `UploadcareUploadClient/${version}/test (JavaScript)`
     )
   })
 
   it('should generate user agent with integration and pub-key', () => {
     expect(getUserAgent({ publicKey: 'test', integration: 'test' })).toBe(
-      'UploadcareUploadClient/1.0.0/test (JavaScript; test)'
+      `UploadcareUploadClient/${version}/test (JavaScript; test)`
     )
   })
 })
