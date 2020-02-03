05cafb2b9d696ec81682faa75ab8a01de36e67ed
jeetiss <jeetiss@yandex.ru>
2020-02-03 21:02:24 +0300

    chore: update shipjs config

--- ship.config.js
+++ ship.config.js
@@ -8,5 +8,19 @@ module.exports = {
   versionUpdated: ({ version, dir }) => {
     const versionPath = path.resolve(dir, 'src/version.ts');
     fs.writeFileSync(versionPath, `export default '${version}'\n`);
+  },
+  slack: {
+    // disable slack notification for `prepared` and `releaseStart` lifecycle.
+    // Ship.js will send slack message only for `releaseSuccess`.
+    prepared: null,
+    releaseStart: null
+  },
+  // skip preparetion if master contain only `chore` commits
+  shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
+    const { fix = 0 } = commitNumbersPerType;
+    if (releaseType === "patch" && fix === 0) {
+      return false;
+    }
+    return true;
   }
 }
