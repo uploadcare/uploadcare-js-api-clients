d81708201ba4058a0df17554bd5400637ecc2b3e
jeetiss <jeetiss@yandex.ru>
2020-04-16 13:22:28 +0300

    refactor: split retry test for better cases

--- test/tools/retry.test.ts
+++ test/tools/retry.test.ts
@@ -3,34 +3,32 @@ import retrier from '../../src/tools/retry'
 describe('retrier', () => {
   test('retry function should work with delay parameter', async () => {
     const a = await retrier(({ attempt, retry }) =>
-      Promise.resolve().then(() => {
-        if (attempt < 10) {
-          return retry(10)
-        } else {
-          return 10
-        }
-      })
+      Promise.resolve().then(() => (attempt < 2 ? retry(10) : 10))
     )
 
     expect(a).toBe(10)
   })
 
   test('retry function should work without delay parameter', async () => {
-    const func = jest.fn()
-
     const a = await retrier(({ attempt, retry }) =>
-      Promise.resolve().then(() => {
-        func()
-        if (attempt < 2) {
-          return retry()
-        } else {
-          return 10
-        }
-      })
+      Promise.resolve().then(() => (attempt < 2 ? retry() : 10))
     )
 
     expect(a).toBe(10)
-    expect(func).toHaveBeenCalledTimes(3)
+  })
+
+  test('attempt parameter should be incremented after every execution', async () => {
+    let i = 0
+    const a = await retrier(({ attempt, retry }) =>
+      Promise.resolve().then(() => {
+        expect(attempt).toBe(i)
+        i += 1
+
+        return attempt < 2 ? retry() : 'result'
+      })
+    )
+
+    expect(a).toBe('result')
   })
 
   test('retrier should throw errors', async () => {
