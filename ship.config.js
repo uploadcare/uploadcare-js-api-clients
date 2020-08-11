9252386ddf9e8dd62116d2c2666a1f16b185b88e
jeetiss <jeetiss@yandex.ru>
2020-08-11 10:37:25 +0300

    chore: setup alpha releases

--- ship.config.js
+++ ship.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
-  mergeStrategy: { toSameBranch: ['master'] },
+  mergeStrategy: { toSameBranch: ['master', 'alpha'] },
   versionUpdated: ({ version, dir }) => {
     const versionPath = path.resolve(dir, 'src/version.ts');
     fs.writeFileSync(versionPath, `export default '${version}'\n`);
