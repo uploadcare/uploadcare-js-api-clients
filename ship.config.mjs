cb22ddcaf547fd44add3af631fb4343b80652099
jeetiss <jeetiss@yandex.ru>
2020-08-25 11:51:45 +0300

    send notifications to @nd0ut

--- ship.config.mjs
+++ ship.config.mjs
@@ -7,7 +7,7 @@ export default {
     const versionPath = resolve(dir, 'src/version.ts');
     writeFileSync(versionPath, `export default '${version}'\n`);
   },
-  pullRequestReviewers: ['jeetiss'],
+  pullRequestReviewers: ['nd0ut'],
   slack: {
     // disable slack notification for `prepared` lifecycle.
     // Ship.js will send slack message only for `releaseSuccess`.
