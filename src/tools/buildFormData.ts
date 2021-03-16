428b039374c98ae689c6c40b3e91d25b11481849
Aleksandr Grenishin <nd0ut.me@gmail.com>
2021-03-16 19:29:09 +0300

    fix(react-native): support direct uploads through FormData (#307)
    * fix: support direct uploads through FormData under react-native runtime
    
    * chore: ignore lgtm warning
    
    * chore: dont use word 'triple' in variable name

--- src/tools/buildFormData.ts
+++ src/tools/buildFormData.ts
@@ -1,4 +1,4 @@
-import getFormData from './getFormData.node'
+import getFormData, { transformFile } from './getFormData.node'
 
 import * as NodeFormData from 'form-data'
 import { BrowserFile, NodeFile } from '../request/types'
@@ -13,20 +13,22 @@ type FileTuple = ['file', BrowserFile | NodeFile, string]
 type BaseType = string | number | void
 type FormDataTuple = [string, BaseType | BaseType[]]
 
+const isFileTuple = (tuple: FormDataTuple | FileTuple): tuple is FileTuple =>
+  tuple[0] === 'file'
+
 function buildFormData(
   body: (FormDataTuple | FileTuple)[]
 ): FormData | NodeFormData {
   const formData = getFormData()
 
-  const isTriple = (tuple: FormDataTuple | FileTuple): tuple is FileTuple =>
-    tuple[0] === 'file'
-
   for (const tuple of body) {
     if (Array.isArray(tuple[1])) {
       // refactor this
       tuple[1].forEach(val => val && formData.append(tuple[0] + '[]', `${val}`))
-    } else if (isTriple(tuple)) {
-      formData.append(tuple[0], tuple[1] as Blob, tuple[2])
+    } else if (isFileTuple(tuple)) {
+      const name = tuple[2]
+      const file = transformFile(tuple[1], name) as Blob // lgtm[js/superfluous-trailing-arguments]
+      formData.append(tuple[0], file, name)
     } else if (tuple[1] != null) {
       formData.append(tuple[0], `${tuple[1]}`)
     }
