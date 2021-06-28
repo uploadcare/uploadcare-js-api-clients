e16dc732613614686048f15fb1dcc8f400663e4d
Aleksandr Grenishin <nd0ut.me@gmail.com>
2021-06-28 12:51:50 +0300

    fix: pass missing props from `uploadFile` down to the upload methods (#339)

--- test/uploadFile/fileFromUrl.test.ts
+++ test/uploadFile/fileFromUrl.test.ts
@@ -3,6 +3,8 @@ import uploadFile from '../../src/uploadFile'
 import { getSettingsForTesting } from '../_helpers'
 import { UploadClientError } from '../../src/tools/errors'
 import CancelController from '../../src/tools/CancelController'
+import * as http from 'http'
+import * as https from 'https'
 
 describe('uploadFrom URL', () => {
   it('should resolves when file is ready on CDN', async () => {
@@ -27,6 +29,44 @@ describe('uploadFrom URL', () => {
     const file = await uploadFile(sourceUrl, settings)
 
     expect(file.isStored).toBeFalsy()
+  })
+
+  it('should accept checkForUrlDuplicates setting', async () => {
+    const sourceUrl = factory.imageUrl('valid')
+    const settings = getSettingsForTesting({
+      publicKey: factory.publicKey('image'),
+      checkForUrlDuplicates: true
+    })
+
+    const isHttpsProtocol = settings.baseURL.includes('https')
+    const spy = jest.spyOn(isHttpsProtocol ? https : http, 'request')
+    await uploadFile(sourceUrl, settings)
+
+    const uploadRequest = spy.mock.calls[0][0]
+    expect(uploadRequest['query']).toEqual(
+      expect.stringContaining('check_URL_duplicates=1')
+    )
+    spy.mockClear()
+  })
+
+  it('should accept saveUrlForRecurrentUploads setting', async () => {
+    const sourceUrl = factory.imageUrl('valid')
+    const settings = getSettingsForTesting({
+      publicKey: factory.publicKey('image'),
+      saveUrlForRecurrentUploads: true
+    })
+
+    const isHttpsProtocol = settings.baseURL.includes('https')
+      ? 'https'
+      : 'http'
+    const spy = jest.spyOn(isHttpsProtocol ? https : http, 'request')
+    await uploadFile(sourceUrl, settings)
+
+    const uploadRequest = spy.mock.calls[0][0]
+    expect(uploadRequest['query']).toEqual(
+      expect.stringContaining('save_URL_duplicates=1')
+    )
+    spy.mockClear()
   })
 
   it('should be able to cancel uploading', async () => {
