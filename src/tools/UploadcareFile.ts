bead2c8eaed28ea9df8d01b52a96bb4c3caade29
nd0ut <nd0ut.me@gmail.com>
2022-05-25 14:45:41 +0300

    feat(UploadcareFile): add `s3Url` property

--- src/tools/UploadcareFile.ts
+++ src/tools/UploadcareFile.ts
@@ -18,6 +18,7 @@ export class UploadcareFile {
   readonly isImage: null | boolean = null
   readonly mimeType: null | string = null
   readonly cdnUrl: null | string = null
+  readonly s3Url: null | string = null
   readonly originalFilename: null | string = null
   readonly imageInfo: null | ImageInfo = null
   readonly videoInfo: null | VideoInfo = null
@@ -35,9 +36,13 @@ export class UploadcareFile {
       fileName?: string
     }
   ) {
-    const cdnUrl = `${baseCDN}/${fileInfo.uuid}/`
+    const { uuid, s3Bucket } = fileInfo
+    const cdnUrl = `${baseCDN}/${uuid}/`
+    const s3Url = s3Bucket
+      ? `https://${s3Bucket}.s3.amazonaws.com/${uuid}/${fileInfo.filename}`
+      : null
 
-    this.uuid = fileInfo.uuid
+    this.uuid = uuid
     this.name = fileName || fileInfo.filename
     this.size = fileInfo.size
     this.isStored = fileInfo.isStored
@@ -49,6 +54,7 @@ export class UploadcareFile {
     this.videoInfo = camelizeKeys(fileInfo.videoInfo)
     this.contentInfo = camelizeKeys(fileInfo.contentInfo)
     this.metadata = fileInfo.metadata || null
-    this.s3Bucket = fileInfo.s3Bucket || null
+    this.s3Bucket = s3Bucket || null
+    this.s3Url = s3Url
   }
 }
