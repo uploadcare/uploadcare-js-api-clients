b8816a25ee86047a47df2c4a524393084bbeb6ad
nd0ut <nd0ut.me@gmail.com>
2022-06-02 16:04:53 +0300

    refactor!: get rid of CommonJS bundles, build ESM only

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,38 @@
 import alias from '@rollup/plugin-alias'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
+import typescript from '@rollup/plugin-typescript'
 
-const config = ({ format, browser }) => ({
+const nodeResolver = nodeResolve()
+
+const config = ({ env }) => ({
   input: 'src/index.ts',
   output: {
     dir: `dist`,
-    format,
-    entryFileNames: [
-      '[name]',
-      browser && 'browser',
-      format === 'esm' ? 'js' : 'cjs'
-    ]
-      .filter(Boolean)
-      .join('.')
+    format: 'esm',
+    entryFileNames: ['[name]', env, 'js'].filter(Boolean).join('.')
   },
   plugins: [
-    browser &&
-      alias({
-        entries: [
-          {
-            find: '../request/request.node',
-            replacement: '../request/request.browser'
-          },
-          {
-            find: '../tools/sockets.node',
-            replacement: '../tools/sockets.browser'
-          },
-          {
-            find: './getFormData.node',
-            replacement: './getFormData.browser'
-          },
-          {
-            find: 'abort-controller',
-            replacement: './node_modules/abort-controller/browser.mjs'
-          }
-        ]
-      }),
-
-    nodeResolve({ browser }),
-
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-      tsconfigOverride: {
-        compilerOptions: {
-          target: format === 'esm' ? 'es6' : 'es5'
+    alias({
+      entries: [
+        {
+          find: /(\.)(node)/,
+          replacement: `$1${env}`,
         }
-      }
+      ]
+    }),
+    nodeResolver,
+    typescript({
+      tsconfig: 'tsconfig.build.json'
     })
   ],
-  external: browser
-    ? []
-    : ['https', 'http', 'stream', 'url', 'form-data', 'ws', 'abort-controller']
+  external:
+    env === 'browser' || env === 'react-native'
+      ? []
+      : ['https', 'http', 'stream', 'url', 'form-data', 'ws']
 })
 
 export default [
-  config({ format: 'cjs', browser: true }),
-  config({ format: 'cjs', browser: false }),
-  config({ format: 'esm', browser: true }),
-  config({ format: 'esm', browser: false })
+  config({ env: 'node' }),
+  config({ env: 'browser' }),
+  config({ env: 'react-native' }),
 ]
