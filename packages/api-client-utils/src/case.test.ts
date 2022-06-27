32d10ca2041691e0642fbbdfecc94c4bfe6bb7a0
nd0ut <nd0ut.me@gmail.com>
2022-06-27 14:41:04 +0300

    chore: fix case test

--- packages/api-client-utils/src/case.test.ts
+++ packages/api-client-utils/src/case.test.ts
@@ -12,24 +12,22 @@ describe('camelize', () => {
 
 describe('camelizeObject', () => {
   it('should work', () => {
-    const input = {
-      foo_bar: 'test1',
-      foo_bar_baz: 'test2',
-      foo: 'test3',
-      Foo_bar_baz_4: { one_more_thing: 'test4' },
-      foo_bar5: [1, 2, 3, 4, 5],
-      foo_bar6: [{ foo_bar: 'baz' }]
-    }
-    const expected = {
+    expect(
+      camelizeObject({
+        foo_bar: 'test1',
+        foo_bar_baz: 'test2',
+        foo: 'test3',
+        Foo_bar_baz_4: { one_more_thing: 'test4' },
+        foo_bar5: [1, 2, 3, 4, 5],
+        foo_bar6: [{ foo_bar: 'baz' }]
+      })
+    ).toEqual({
       fooBar: 'test1',
       fooBarBaz: 'test2',
       foo: 'test3',
       fooBarBaz4: { oneMoreThing: 'test4' },
       fooBar5: [1, 2, 3, 4, 5],
       fooBar6: [{ fooBar: 'baz' }]
-    }
-    expect(camelizeObject(input)).toEqual({
-      expected
     })
   })
 })
