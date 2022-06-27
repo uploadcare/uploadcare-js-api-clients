db79b2d18403a646db3e71759b693ebc7aecfef6
nd0ut <nd0ut.me@gmail.com>
2022-06-27 19:08:25 +0300

    chore: improve RestClientError

--- packages/rest-client/src/tools/RestClientError.ts
+++ packages/rest-client/src/tools/RestClientError.ts
@@ -6,6 +6,9 @@ export type RestClientErrorOptions = {
 const DEFAULT_MESSAGE = 'Unknown error'
 
 export class RestClientError extends Error {
+  readonly status?: number
+  readonly statusText?: string
+
   readonly request?: Request
   readonly response?: Response
 
@@ -20,6 +23,17 @@ export class RestClientError extends Error {
     this.request = options.request
     this.response = options.response
 
+    this.status = options.response?.status
+    this.statusText = options.response?.statusText
+
     Object.setPrototypeOf(this, RestClientError.prototype)
+  }
+
+  toString() {
+    const status =
+      this.status || this.statusText
+        ? `[${[this.status, this.statusText].filter(Boolean).join(' ')}]`
+        : ''
+    return `${this.name}${status}: ${this.message}`
   }
 }
