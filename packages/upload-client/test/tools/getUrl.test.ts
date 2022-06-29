cbf5472507de4458fccfa98304326e6762b43171
nd0ut <nd0ut.me@gmail.com>
2022-06-29 03:51:08 +0300

    fix: handle falsy values while creating query string, use URLSearchParams to build it

--- packages/upload-client/test/tools/getUrl.test.ts
+++ packages/upload-client/test/tools/getUrl.test.ts
@@ -19,7 +19,9 @@ describe('create URL', () => {
         lol: 'param',
         kek: ['param1', 'param2']
       })
-    ).toBe('https://github.com/base/?lol=param&kek[]=param1&kek[]=param2')
+    ).toBe(
+      'https://github.com/base/?lol=param&kek%5B%5D=param1&kek%5B%5D=param2'
+    )
   })
 
   it('should escape url', () => {
@@ -33,16 +35,29 @@ describe('create URL', () => {
   it('should accept objects', () => {
     expect(
       getUrl('https://github.com', '/base/', {
-        lol: 'param',
-        kek: {
-          key1: 'value1',
-          key2: 2,
-          key3: undefined,
-          key4: null as never
+        key0: 'value0',
+        key1: {
+          deepKey0: 'deepValue0',
+          deepKey1: 1,
+          deepKey2: undefined,
+          deepKey3: null,
+          deepKey4: ''
         }
       })
     ).toBe(
-      'https://github.com/base/?lol=param&kek[key1]=value1&kek[key2]=2&kek[key4]=null'
+      'https://github.com/base/?key0=value0&key1%5BdeepKey0%5D=deepValue0&key1%5BdeepKey1%5D=1'
     )
+  })
+
+  it('should ignore falsy values', () => {
+    expect(
+      getUrl('https://github.com', '/base/', {
+        key0: 'value0',
+        key1: null,
+        key2: undefined,
+        key3: 0,
+        key4: ''
+      })
+    ).toBe('https://github.com/base/?key0=value0&key3=0')
   })
 })
