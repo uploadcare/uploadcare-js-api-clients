a942c2ab131258ee839075c913d03b0b39489c37
nd0ut <nd0ut.me@gmail.com>
2022-06-29 03:25:36 +0300

    chore: typos

--- packages/upload-client/test/tools/getUrl.test.ts
+++ packages/upload-client/test/tools/getUrl.test.ts
@@ -2,37 +2,37 @@ import getUrl from '../../src/tools/getUrl'
 
 describe('create URL', () => {
   it('should create url', () => {
-    expect(getUrl('https:/github.com', '/base/')).toBe(
-      'https:/github.com/base/'
+    expect(getUrl('https://github.com', '/base/')).toBe(
+      'https://github.com/base/'
     )
   })
 
   it('should work with query', () => {
     expect(
-      getUrl('https:/github.com', '/base/', { lol: 'param', kek: 'param' })
-    ).toBe('https:/github.com/base/?lol=param&kek=param')
+      getUrl('https://github.com', '/base/', { lol: 'param', kek: 'param' })
+    ).toBe('https://github.com/base/?lol=param&kek=param')
   })
 
-  it('query should accept arrays', () => {
+  it('should accept arrays', () => {
     expect(
-      getUrl('https:/github.com', '/base/', {
+      getUrl('https://github.com', '/base/', {
         lol: 'param',
         kek: ['param1', 'param2']
       })
-    ).toBe('https:/github.com/base/?lol=param&kek[]=param1&kek[]=param2')
+    ).toBe('https://github.com/base/?lol=param&kek[]=param1&kek[]=param2')
   })
 
-  it('query should escape url', () => {
+  it('should escape url', () => {
     expect(
-      getUrl('https:/github.com', '/base/', {
-        lol: 'https:/github.com'
+      getUrl('https://github.com', '/base/', {
+        lol: 'https://github.com'
       })
-    ).toBe('https:/github.com/base/?lol=https%3A%2Fgithub.com')
+    ).toBe('https://github.com/base/?lol=https%3A%2F%2Fgithub.com')
   })
 
-  it('query should accept objects', () => {
+  it('should accept objects', () => {
     expect(
-      getUrl('https:/github.com', '/base/', {
+      getUrl('https://github.com', '/base/', {
         lol: 'param',
         kek: {
           key1: 'value1',
@@ -42,7 +42,7 @@ describe('create URL', () => {
         }
       })
     ).toBe(
-      'https:/github.com/base/?lol=param&kek[key1]=value1&kek[key2]=2&kek[key4]=null'
+      'https://github.com/base/?lol=param&kek[key1]=value1&kek[key2]=2&kek[key4]=null'
     )
   })
 })
