f9cd4cc840441860b0a74288fa9888affe29309d
nd0ut <nd0ut.me@gmail.com>
2022-12-29 19:45:06 +0400

    chore(upload-client): fix `sliceChunk`

--- packages/upload-client/src/uploadFile/sliceChunk.ts
+++ packages/upload-client/src/uploadFile/sliceChunk.ts
@@ -1,15 +1,13 @@
 import { Sliceable } from '../types'
 
-type Sliceable<T> = T & { slice: (start: number, end: number) => Sliceable<T> }
-
 export const sliceChunk = <T extends Sliceable>(
-  file: Sliceable<T>,
+  file: T,
   index: number,
   fileSize: number,
   chunkSize: number
-): Sliceable<T> => {
+): T => {
   const start = chunkSize * index
   const end = Math.min(start + chunkSize, fileSize)
 
-  return file.slice(start, end) as Sliceable<T>
+  return file.slice(start, end) as T
 }
