0efeee7e15a2fe3ce9cf13d45ac3526b7a1e897b
nd0ut <nd0ut.me@gmail.com>
2023-03-09 14:01:29 +0400

    chore(signed-uploads): add jsdoc annotation

--- packages/signed-uploads/src/generateSecureSignature.ts
+++ packages/signed-uploads/src/generateSecureSignature.ts
@@ -8,6 +8,19 @@ export type GenerateSecureSignatureOptions =
       lifetime: number
     }
 
+/**
+ * Generate a secure signature for uploading a file to Uploadcare.
+ * The signature is used for signing the upload request.
+ * This is the simplest way to secure your upload API requests.
+ * You can read more about this here:
+ *
+ * @see https://uploadcare.com/docs/security/secure-uploads/
+ *
+ * @param secretKey - The secret key used to generate the signature.
+ * @param options - Additional options for the signature.
+ * @param options.expire - The expiration timestamp of the signature.
+ * @param options.lifetime - The lifetime of the signature.
+ */
 export const generateSecureSignature = (
   secret: string,
   options: GenerateSecureSignatureOptions
