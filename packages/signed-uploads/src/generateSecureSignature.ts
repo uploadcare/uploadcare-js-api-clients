3c16d05beaa0d38bf95bd5e4386c26921e083d2c
nd0ut <nd0ut.me@gmail.com>
2023-03-09 14:58:24 +0400

    chore(signed-uploads): convert js time to unixtime

--- packages/signed-uploads/src/generateSecureSignature.ts
+++ packages/signed-uploads/src/generateSecureSignature.ts
@@ -2,24 +2,25 @@ import { createHmac } from 'crypto'
 
 export type GenerateSecureSignatureOptions =
   | {
-      expire: number | Date
+      /**
+       * The expiration timestamp of the signature in milliseconds since the
+       * epoch
+       */
+      expire: number
     }
   | {
+      /** The lifetime of the signature in milliseconds */
       lifetime: number
     }
 
+const msToUnixTimestamp = (ms: number) => Math.floor(ms / 1000)
+
 /**
- * Generate a secure signature for uploading a file to Uploadcare.
- * The signature is used for signing the upload request.
- * This is the simplest way to secure your upload API requests.
- * You can read more about this here:
- *
- * @see https://uploadcare.com/docs/security/secure-uploads/
+ * Generate a secure signature for signing the upload request to Uploadcare.
  *
  * @param secretKey - The secret key used to generate the signature.
- * @param options - Additional options for the signature.
- * @param options.expire - The expiration timestamp of the signature.
- * @param options.lifetime - The lifetime of the signature.
+ * @param options - Expiration options for the signature.
+ * @see https://uploadcare.com/docs/security/secure-uploads/
  */
 export const generateSecureSignature = (
   secret: string,
@@ -27,8 +28,8 @@ export const generateSecureSignature = (
 ) => {
   const expire =
     'expire' in options
-      ? new Date(options.expire).getTime()
-      : Date.now() + options.lifetime
+      ? msToUnixTimestamp(new Date(options.expire).getTime())
+      : msToUnixTimestamp(Date.now() + options.lifetime)
 
   const hmac = createHmac('sha256', secret)
   hmac.update(expire.toString())
