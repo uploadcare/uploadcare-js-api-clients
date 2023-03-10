97f7ebb796b3c2d38ca9ddbccb07fb706ed75eba
nd0ut <nd0ut.me@gmail.com>
2023-03-10 10:46:38 +0400

    feat(signed-uploads): update `generateSecureSignature` signature - BREAKING CHANGE without major bump

--- packages/signed-uploads/src/generateSecureSignature.test.ts
+++ packages/signed-uploads/src/generateSecureSignature.test.ts
@@ -6,23 +6,43 @@ const FIXTURE_DATE = new Date(1678359840000)
 const FIXTURE_LIFETIME = 60 * 30 * 1000 // 30 minutes
 const EXPECTED_SIGNATURE =
   '93b69d086a487fbdfc36172b96a5d7f5afa7cb209e43e5f25890bc037e638584'
+const EXPECTED_EXPIRE = '1678361640'
 
 describe('generateSecureSignature', () => {
   beforeAll(() => {
     jest.useFakeTimers().setSystemTime(FIXTURE_DATE)
   })
 
-  it('should return signature by `expire` as number', () => {
-    const signature = generateSecureSignature(FIXTURE_SECRET, {
-      expire: Date.now() + FIXTURE_LIFETIME
-    })
-    expect(signature).toBe(EXPECTED_SIGNATURE)
+  it('should return signature and expire by `expire` as number', () => {
+    const { secureSignature, secureExpire } = generateSecureSignature(
+      FIXTURE_SECRET,
+      {
+        expire: Date.now() + FIXTURE_LIFETIME
+      }
+    )
+    expect(secureSignature).toBe(EXPECTED_SIGNATURE)
+    expect(secureExpire).toBe(EXPECTED_EXPIRE)
+  })
+
+  it('should return signature and expire by `expire` as Date', () => {
+    const { secureSignature, secureExpire } = generateSecureSignature(
+      FIXTURE_SECRET,
+      {
+        expire: new Date(FIXTURE_DATE.getTime() + FIXTURE_LIFETIME)
+      }
+    )
+    expect(secureSignature).toBe(EXPECTED_SIGNATURE)
+    expect(secureExpire).toBe(EXPECTED_EXPIRE)
   })
 
   it('should return signature by `lifetime`', () => {
-    const signature = generateSecureSignature(FIXTURE_SECRET, {
-      lifetime: FIXTURE_LIFETIME
-    })
-    expect(signature).toBe(EXPECTED_SIGNATURE)
+    const { secureSignature, secureExpire } = generateSecureSignature(
+      FIXTURE_SECRET,
+      {
+        lifetime: FIXTURE_LIFETIME
+      }
+    )
+    expect(secureSignature).toBe(EXPECTED_SIGNATURE)
+    expect(secureExpire).toBe(EXPECTED_EXPIRE)
   })
 })
