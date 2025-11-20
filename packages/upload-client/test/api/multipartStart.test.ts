4c626b4c9305f1b8d5cc70fa7440b658e63b386e
Egor Didenko <didenk.egor@gmail.com>
2025-11-20 16:15:26 -0500

    test: fixed multipartStart.test.ts

--- packages/upload-client/test/api/multipartStart.test.ts
+++ packages/upload-client/test/api/multipartStart.test.ts
@@ -3,7 +3,7 @@ import * as factory from '../_fixtureFactory'
 import { getSettingsForTesting } from '../_helpers'
 import { UploadError } from '../../src/tools/UploadError'
 
-describe.skip('API - multipartStart', () => {
+describe('API - multipartStart', () => {
   const size = factory.file(12).size
 
   it('should be able to start upload data', async () => {
@@ -44,10 +44,10 @@ describe.skip('API - multipartStart', () => {
 
     const upload = multipartStart(size, settings)
 
-    await expect(upload).rejects.toThrowError(
-      new UploadError(
-        'File size can not be less than 10485760 bytes. Please use direct upload instead of multipart.'
-      )
+    await expect(upload).rejects.toThrow(UploadError)
+
+    await expect(upload).rejects.toThrow(
+      /File size can not be less than \d+ bytes\. Please use direct upload instead of multipart\./
     )
   })
 
